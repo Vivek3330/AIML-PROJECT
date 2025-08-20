a430f95c61a490521a75371972bb2ccfb5d21e75
Vivek3330 <vivekbandikatla@gmail.com>
2025-08-20 19:43:10 +0530

    Update ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -31,4 +31,5 @@ pipeline.fit(X_train, y_train)
 with open("model.pkl", "wb") as f:
     pickle.dump(pipeline, f)
 
-print("✅ Model trained and saved as model.pkl")
+print("Model trained and saved as model.pkl")
+
